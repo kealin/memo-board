0c324ab72750abcdf7fdcb21794a0bcdf3a2792e
Kim Lindqvist <Kim Lindqvist>
2016-08-28 20:51:05 +0300

    Changed notification location, removed scroll bars where not needed

--- memo-app/app/app.module.js
+++ memo-app/app/app.module.js
@@ -4,3 +4,15 @@ var app = angular.module('app',
 'ui.bootstrap',
 'ui-notification'
 ]);
+
+app.config(function(NotificationProvider) {
+	NotificationProvider.setOptions({
+		delay: 10000,
+		startTop: 20,
+		startRight: 10,
+		verticalSpacing: 20,
+		horizontalSpacing: 20,
+		positionX: 'right',
+		positionY: 'bottom'
+	});
+});
